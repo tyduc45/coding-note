8f04f2ea4679260f98118d27c67974e3567660a5
tyduc45 <binaryhaze098@gmail.com>
2026-08-10 23:50:39 +1000

    daily checkout

--- 91.decode-ways.cpp
+++ 91.decode-ways.cpp
@@ -11,17 +11,16 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
+        int n = s.length();
         vector<int> f(n + 1 , 0);
         f[0] = 1;
-        // i 代表的是当前处理的长度
-        for(int i = 1 ; i <= n ; i++)
+        for(int i = 1; i <= n ;i++)
         {
             if(s[i - 1] != '0')
             {
                 f[i] += f[i - 1];
             }
-            if(i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + s[i - 1] - '0') <= 26)
+            if(i > 1 && s[i - 2] != '0' && ((s[i - 2] - '0') * 10 + (s[i - 1] -'0') <= 26))
             {
                 f[i] += f[i - 2];
             }
---
+++ leetcode_cpp_debug/all_sub_string.cpp
@@ -0,0 +1,51 @@
+// http://nowcoder.com/practice/92e6247998294f2c933906fdedbc6e6a
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param s string字符串 
+     * @return string字符串vector
+     */
+    string turn_into_string(vector<char> &path , int size)
+    {
+        string builder = "";
+        for(int i = 0 ; i < size ; i++)
+        {
+            builder += path[i];
+        }
+        return builder;
+    }
+    void f(string &s , int i , vector<char> &path , int size , set<string> & set)
+    {
+        if(i == s.length())
+        {
+            string builder = turn_into_string(path, size);
+            set.insert(builder);
+        }
+        else 
+        {
+            path[size] = s[i];
+            f(s , i + 1 , path , size + 1 , set);
+            f(s , i + 1 , path , size , set);
+        }
+    }
+    vector<string> generatePermutation(string s) {
+        // write code here
+        vector<string> ans;
+        int n = s.length();
+        set<string> hash_set;
+        vector<char> path(n , '0');
+        f(s, 0 , path , 0 , hash_set);
+        for(const auto & elem : hash_set)
+        {
+            ans.push_back(elem);
+        }
+        return ans;
+    }
+};
